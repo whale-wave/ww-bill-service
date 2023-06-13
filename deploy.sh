8b9ea17349df759bf1bb2b70c631040126648964
Avan <layouwen@gmail.com>
2023-06-13 11:17:15 +0800

    ci: remove none image

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-git pull origin master
+git pull
 pwd
 cd ../ww-bill-client
 pwd
-git pull origin master
+git pull
 cd ../ww-bill-service
 
+docker image prune -f
 docker-compose down
 docker-compose up --build -d
