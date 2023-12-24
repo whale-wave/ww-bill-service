ffae6b93873c5c4f8bc0a752d17d1dbd43298ed5
Avan <layouwen@gmail.com>
2023-12-24 14:37:41 +0800

    chore: simple config

--- ormconfig.js
+++ ormconfig.js
@@ -1,26 +1,25 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const baseConfig = require('./ormconfig.base.js');
+const {
+  ORM_TYPE = 'postgres',
+  ORM_HOST = 'localhost',
+  ORM_PORT = '5432',
+  ORM_USERNAME = 'root',
+  ORM_PASSWORD = '',
+  ORM_DATABASE = 'development',
+  ORM_SYNCHRONIZE = 'false',
+  ORM_TIMEZONE = '+08:00',
+} = process.env;
 
 module.exports = [
   {
-    ...baseConfig.development,
+    type: ORM_TYPE,
+    host: ORM_HOST,
+    port: isNaN(Number(ORM_PORT)) ? '5432' : Number(ORM_PORT),
+    username: ORM_USERNAME,
+    password: ORM_PASSWORD,
+    database: ORM_DATABASE,
+    synchronize: ORM_SYNCHRONIZE === 'true',
+    timezone: ORM_TIMEZONE,
     entities: ['dist/**/*.entity{.ts,.js}'],
-    migrations: ['dist/migrations/*{.ts,.js}'],
-    cli: {
-      migrationsDir: './src/migrations',
-    },
-  },
-  {
-    ...baseConfig.test,
-    name: 'test',
-    migrations: ['dist/migrations/*{.ts,.js}'],
-    cli: {
-      migrationsDir: './src/migrations',
-    },
-  },
-  {
-    ...baseConfig.production,
-    name: 'prod',
     migrations: ['dist/migrations/*{.ts,.js}'],
     cli: {
       migrationsDir: './src/migrations',
