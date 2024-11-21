eedf1e283c26b3dbb56c362605cffcb0628947f6
Avan <layouwen@gmail.com>
2024-11-21 11:57:53 +0800

    chore: remove user entity

--- src/modules/asset/asset.module.ts
+++ src/modules/asset/asset.module.ts
@@ -2,13 +2,12 @@ import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AssetEntity, AssetGroupEntity, AssetRecordEntity } from '../../entity';
 import { UserModule } from '../user/user.module';
-import { User } from '../user/entity/user.entity';
 import { AssetService } from './asset.service';
 import { AssetController } from './asset.controller';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([AssetEntity, AssetGroupEntity, AssetRecordEntity, User]),
+    TypeOrmModule.forFeature([AssetEntity, AssetGroupEntity, AssetRecordEntity]),
     forwardRef(() => UserModule),
   ],
   controllers: [AssetController],
